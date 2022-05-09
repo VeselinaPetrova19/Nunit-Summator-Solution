79337cb8b1234fb54a9fa0f4826dbd2cea232dae
Veselina Petrova <99814999+VeselinaPetrova19@users.noreply.github.com>
2022-05-09 15:48:41 +0300

    Initial_Commit

---
+++ Summator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Summator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6 });
+           Console.WriteLine(sum);   
+            
+           if(sum == 21)
+            {
+                Console.WriteLine("Test PASS");
+            } else
+            {
+                Console.WriteLine("Test FAIL");
+            }
+
+            double average = Summator.Average(new int[] { 7, 8, 9, 10 });
+            Console.WriteLine(average.ToString());
+            if(average == 8.5)
+            {
+                Console.WriteLine("Test PASS");
+            } else
+            {
+                Console.WriteLine("Test FAIL");
+            }
+        }
+    }
+}
---
+++ Summator/Summator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator
+{
+    public class Summator
+    {
+        public static long Sum(int[] arr)
+        {
+            long sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum; 
+        }
+        public static double Average(int[] arr)
+        {
+            double average = 0; 
+            for(int i = 0; i < arr.Length; i++)
+            {
+                average += arr[i];
+            }
+            return average / arr.Length;
+        }
+    }
+}
